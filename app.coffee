462be02bbffa84d12f4885c12ddfa2325960b7a5
jkresner <jkresner@yahoo.com.au>
2013-02-25 17:24:32 -0800

    improving about page

--- app.coffee
+++ app.coffee
@@ -3,13 +3,13 @@ console.log "in app node file"
 express = require 'express'
 app = express()
 
-app.get '/', (req, res) -> res.redirect "http://codereview.airpair.co/"
-app.get '/index', (req, res) -> res.sendfile './public/index.html'
-app.get '/about', (req, res) -> res.sendfile './public/about.html'
+app.get '/', (req, res) -> res.sendfile './public/index.html'
+app.get '/about', (req, res) -> res.sendfile './public/index.html'
 app.get '/admin', (req, res) -> res.sendfile './public/admin.html'
 app.get '/review', (req, res) -> res.sendfile './public/review.html'
+app.get '/be-an-expert', (req, res) -> res.sendfile './public/beexpert.html'
 app.get '/become-an-expert', (req, res) -> res.sendfile './public/beexpert.html'
-
+app.get '/find-an-expert', (req, res) -> res.sendfile './public/findexpert.html'
 
 app.use(express.static __dirname+'/public')
 
--- server.js
+++ server.js
@@ -5,15 +5,11 @@ express = require('express');
 app = express();
 
 app.get('/', function(req, res) {
-  return res.redirect("http://codereview.airpair.co/");
-});
-
-app.get('/index', function(req, res) {
   return res.sendfile('./public/index.html');
 });
 
 app.get('/about', function(req, res) {
-  return res.sendfile('./public/about.html');
+  return res.sendfile('./public/index.html');
 });
 
 app.get('/admin', function(req, res) {
@@ -24,8 +20,16 @@ app.get('/review', function(req, res) {
   return res.sendfile('./public/review.html');
 });
 
+app.get('/be-an-expert', function(req, res) {
+  return res.sendfile('./public/beexpert.html');
+});
+
 app.get('/become-an-expert', function(req, res) {
   return res.sendfile('./public/beexpert.html');
+});
+
+app.get('/find-an-expert', function(req, res) {
+  return res.sendfile('./public/findexpert.html');
 });
 
 app.use(express["static"](__dirname + '/public'));
