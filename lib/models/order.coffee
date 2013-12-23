jkresner/airpair.com-v0

commit 7e1804619fdbab7991b8e42be02ba9bab7f71b1a

WIP for new Order.lineItem redeemedCalls schema and tests

lib/models/order.coffee
@@ -32,7 +32,7 @@ schema = new Schema
   requestId:      { required: true, type: ObjectId, ref: 'Request' }
   userId:         { required: true, type: ObjectId, ref: 'User'    }
   company:        { required: true, type: Mixed  }
-  lineItems:      { type: [LineItem] }
+  lineItems:      Array
   utc:            { type: Date, default: Date }
   total:          { required: true, type: Number   }
   profit:         { required: true, type: Number   }

lib/services/requestCalls.coffee
@@ -14,9 +14,11 @@ module.exports = class RequestCallsService
 
   getByExpertId: (expertId, callback) => throw new Error 'not imp'
 
+  # TODO make this a client-side require-able function
   _canScheduleCall: (orders, call) =>
     expertTotal = 0
     expertRedeemed = 0
+
     orders.map (order) =>
       order.lineItems.filter (lineItem) =>
         _.idsEqual lineItem.suggestion.expert._id, call.expertId
@@ -28,19 +30,51 @@ module.exports = class RequestCallsService
 
     call.duration <= expertBalance
 
+  _qtyRemaining: (lineItem) ->
+    lineItem.qty - @sum _.pluck lineItem.redeemedCalls, 'qtyRedeemed'
+
+  # TODO another function to reduce the duration of a call / subtracting hours from orders
+  _modifyOrdersWithCallDuration: (orders, call) =>
+    allocatedSoFar = 0
+    done = false
+    modified = []
+    for order in orders
+      if done then break
+      order.lineItems.filter (lineItem) =>
+        _.idsEqual lineItem.suggestion.expert._id, call.expertId
+      .map (lineItem) =>
+        if done then return
+        redeemedCall = { callId: call._id, qtyRedeemed: 0, qtyCompleted: 0 }
+        lineItem.redeemedCalls.push redeemedCall
+        allocated = Math.min call.duration, @_qtyRemaining(lineItem)
+        allocatedSoFar += allocated
+        redeemedCall.qtyRedeemed += allocated
+        modified.push order
+        done = allocatedSoFar == call.duration
+    modified
+
+  _saveOrdersWithCallDuration: (orders, call) =>
+    modified = _modifyOrdersWithCallDuration orders, call
+
+    saveOrder = (order, cb) ->
+      Order.findByIdAndUpdate order._id, order, cb
+    async.map modified, saveOrder, callback
+
   create: (userId, requestId, call, callback) =>
 
-    # TODO make this a client-side require-able function
+    # change oldest orders first
+    Order.find { requestId }, { sort: 'utc' }, (err, orders) =>
 
-    Order.find { requestId }, (err, orders) =>
       if err then return callback err
 
       if !@_canScheduleCall orders, call
         message = 'Not enough hours: buy more or cancel unfulfilled calls.'
         return callback new Error message
 
-      # TODO update order
-      Request.findByIdAndUpdate requestId, $push: calls: call, callback
+      tasks =
+        request: (cb) -> Request.findByIdAndUpdate requestId, $push: calls: call, cb
+        orders: (cb) -> _saveOrdersWithCallDuration orders, call, cb
+      async.parallel tasks, callback
 
   sum: (list) ->
     add = (prev, cur) ->

test/server/services/requestCalls.coffee
@@ -50,14 +50,21 @@ describe "RequestCallsService", ->
 
   it "can book a 1hr call using 1 order and 1 available lineitem for paul", ->
     call = data.calls[1] # expert is paul
-    orders = [data.orders[5]] # expert is paul, 2 line items
+    orders = [_.clone data.orders[5]] # expert is paul, 2 line items
     expect(svc._canScheduleCall orders, call).to.be.true
 
-  it "can book a 1hr call using 1 order and 1 available lineitem", (done) ->
+    modified = svc._modifyOrdersWithCallDuration orders, call
+    expect(modified).to.have.length 1
+    expect(modified[0].lineItems[0].redeemedCalls[0].qtyRedeemed).to.equal 1
+
+  it.only "can book a 1hr call using 1 order and 1 available lineitem", (done) ->
     @timeout 0
     call = data.calls[1] # expert is paul
-    orders = [data.orders[5]] # expert is paul, 2 line items
-    runCreateCallSuccess orders, call, done
+    orders = [_.clone data.orders[5]] # expert is paul, 2 line items
+    runCreateCallSuccess orders, call, (err, results) ->
+      order = results.orders[0]
+      expect(order.lineItems[0].redeemedCalls[0].qtyRedeemed).to.equal 1
+      done()
 
   it "can book a 2hr call given 2 orders and 2 available lineItems", (done) ->
     @timeout 0

commit 9e03b5fb6dcfaa0e4e802e215d71224ad6d8a982

models/order: fix schema bug introduced by 7e1804619fdbab7991b8e42be02ba9bab7f71b1a

lib/models/order.coffee
@@ -32,7 +32,7 @@ schema = new Schema
   requestId:      { required: true, type: ObjectId, ref: 'Request' }
   userId:         { required: true, type: ObjectId, ref: 'User'    }
   company:        { required: true, type: Mixed  }
-  lineItems:      Array
+  lineItems:      { type: [LineItem] }
   utc:            { type: Date, default: Date }
   total:          { required: true, type: Number   }
   profit:         { required: true, type: Number   }