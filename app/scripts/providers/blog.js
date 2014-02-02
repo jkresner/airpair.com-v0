17ec5248f8b12174ed4fe78f942edf8ecca46030
Jonathon Kresner <jkresner@gmail.com>
2014-02-02 15:09:25 -0800

    Improving figure callout to work for multiple page elements

--- app/scripts/providers/blog.js
+++ app/scripts/providers/blog.js
@@ -1,15 +1,15 @@
 module.exports = function()
 {
-  jQuery('.trackCustomerLogin').each(function (e) {
-    $btn = jQuery(this);
+  jQuery('.trackCustomerLogin').each(function (idx, btn) {
+    $btn = jQuery(btn);
     callToActionHtml = $btn.html()
 
     $btn.mouseover(function (emover) {
       $btn.html("Sign in with Google for Video Chat");
-      $btn.next('except').html('We use your google account for G+ Hangouts')
+      $btn.insertAfter('<figure>We use your google account for G+ Hangouts</figure>')
     }).mouseout(function (emout) {
       $btn.html(callToActionHtml);
-      $btn.next('except').html('')
+      $btn.next('figure').remove()
     })
   });
 }
