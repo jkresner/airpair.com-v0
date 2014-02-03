a8e29ae33f4627a7367f644e423bb0c83038c0f3
Jonathon Kresner <jkresner@gmail.com>
2014-02-02 16:12:30 -0800

    Booom hah

--- app/scripts/providers/blog.js
+++ app/scripts/providers/blog.js
@@ -2,16 +2,15 @@ module.exports = function()
 {
   jQuery('.trackCustomerLogin').each(function (idx) {
     this.$btn = jQuery(this);
-    this.$btn.callToActionHtml = this.$btn.html()
-
     this.$btn.mouseover(function (emover) {
       btn = jQuery(this)
+      jQuery(this).data('act', btn.html())
       btn.html("Sign in with Google for Video Chat");
-      jQuery('<figure>We use your google account for G+ Hangouts <img src="//airpair-blog.s3.amazonaws.com/wp-content/uploads/2014/01/Node.js-Pair-Programming-Help-On-AirPair-300x176.png"></figure>').insertAfter(btn)
+      jQuery('<figure class="gSignin"><p>We use your google account for G+ Hangouts</p><img src="//airpair-blog.s3.amazonaws.com/wp-content/uploads/2014/01/Node.js-Pair-Programming-Help-On-AirPair-300x176.png"></figure>').insertAfter(btn)
     }).mouseout(function (emout) {
       btn = jQuery(this)
-      btn.html(btn.callToActionHtml);
+      btn.html(jQuery(this).data('act'));
       btn.next('figure').remove()
-    })
+    });
   });
 }
