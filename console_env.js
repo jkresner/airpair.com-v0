c229335486c29f8a6747a1bd2c01648f9f08b613
elkelk <danielshaneupton@gmail.com>
2014-07-15 18:05:46 -0400

    Move batch-scripts to scripts
    - Move console script to scripts dir

--- console_env.js
+++
@@ -1,9 +0,0 @@
-require("coffee-script");
-require('./lib/util/globals');
-global.mongoose = require('./app_mongoose')();
-models = require('./lib/models/index');
-for(key in models) {
-  global[key] = models[key];
-}
-repl = require("repl")
-r = repl.start("node> ")
